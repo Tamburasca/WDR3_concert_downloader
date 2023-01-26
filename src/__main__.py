c4920cbcfcffc85b89bc7327d86bc0756a8c1195
Tamburasca <rtimmermann@astro.uni-bonn.de>
2023-01-26 16:43:17 +0100

    1st commit

---
+++ src/__main__.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Downloads mp3 files from a site where the WDR3 concert player is located if
+there's only an afterhearing option of 30 days and, hence, no download button
+available. Copy the url of the site, where the concert resides and run the code:
+$ python3 WDR3_concert_downloader/ <url> -o <file.mp3>
+where e.g.
+url = https://www1.wdr.de/radio/wdr3/programm/sendungen/wdr3-konzert/konzertplayer-klassik-tage-alter-musik-in-herne-concerto-romano-alessandro-quarta-100.html
+Note: if there are multiple mp3 media objects available on the provided website,
+the files downloaded are named in following order:
+file.mp3, file(1).mp3, file(2).mp3, etc. according to the objects encountered in
+the html soup scan.
+"""
+
+import argparse
+from concert_downloader import wdr3_scraper
+
+__author__ = "Dr. Ralf Antonius Timmermann"
+__copyright__ = "Copyright (c) 2022, Dr. Ralf Antonius Timmermann All rights reserved."
+__credits__ = []
+__license__ = "BSD-3-Clause"
+__version__ = "0.4"
+__maintainer__ = "Dr. Ralf Antonius Timmermann"
+__email__ = "rtimmermann@astro.uni-bonn.de"
+__status__ = "Prod"
+
+parser = argparse.ArgumentParser(
+    description="Downloads concert mp3 audio files from WDR3 sites.")
+parser.add_argument(
+    '-o', '--output',
+    required=True,
+    help='Output file (.mp3)')
+parser.add_argument('url',
+                    help='URL of concert player')
+
+exit(
+    wdr3_scraper(url=parser.parse_args().url,
+                 file=parser.parse_args().output)
+)
---
+++ src/concert_downloader.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from pydantic import BaseModel, HttpUrl
+
+
+class TestURL(BaseModel):
+    url: HttpUrl
+
+
+def wdr3_scraper(url: str, file: str) -> int:
+    counter = 0
+    try:
+        if not file.endswith(".mp3"):
+            raise NameError
+        # verificare ed tentare d'aprire initial url
+        res = requests.get(url=TestURL(url=url).url)
+        soup = BeautifulSoup(res.text, "html.parser")
+        for value in soup.find_all('a'):
+            data_ext = value.get('data-extension')
+            if data_ext:
+                sneak = json.loads(data_ext)['mediaObj']['url']
+                # apri ogni javascript media object e taglia non-JSON sui due lati
+                _res = requests.get(url=sneak).text
+                s = json.loads(
+                    _res.lstrip("$mediaObject.jsonpHelper.storeAndPlay(").rstrip(");"))
+                mp3_url = "https:{}".format(s['mediaResource']['dflt']['audioURL'])
+                # apri mp3 object e download binary content to file
+                doc = requests.get(url=mp3_url)
+                file_download = file if counter == 0 else "{}({}).mp3".format(file.rsplit(".", 1)[0],
+                                                                              counter)
+                with open(file_download, 'wb') as f:
+                    f.write(doc.content)
+                print("{1} downloaded to {0} successfully".format(file_download,
+                                                                  mp3_url))
+                counter += 1
+        return 0
+    except NameError:
+        print("Error: filename '{}' is incorrect.".format(file))
+    except Exception as e:
+        print("Occurred error: {}".format(str(e)))  # Bih, chi camurrìa!
+    return 1
