19b26853135beeb510b56a60f45778115ca2c0a3
Tamburasca <rtimmermann@astro.uni-bonn.de>
2023-06-22 16:13:15 +0200

    version 1.3.2

--- WDR3_concert_downloader/concert_downloader.py
+++ WDR3_concert_downloader/concert_downloader.py
@@ -13,25 +13,28 @@ def wdr3_scraper(url: str, file: str) -> int:
     try:
         if not file.endswith(".mp3"):
             raise NameError
-        # verificare ed tentare d'aprire url iniziale
+        # verificare e tentare d'aprire url iniziale
         response = requests.get(url=TestURL(url=url).url)
+        response.raise_for_status()
         soup = BeautifulSoup(response.text, "html.parser")
         for value in soup.find_all('a'):
             data_ext = value.get('data-extension')
             if data_ext:
                 sneak = json.loads(data_ext)['mediaObj']['url']
-                # apri ogni javascript media object e taglia non-JSON sui due lati
-                res = requests.get(url=sneak).text
+                # apri ogni l'oggetto javascript media e taglia non-JSON sui due lati
+                res = requests.get(url=sneak)
+                res.raise_for_status()
                 s = json.loads(
-                    res.lstrip(
+                    res.text.lstrip(
                         "$mediaObject.jsonpHelper.storeAndPlay("
                     ).rstrip(");")
                 )
                 mp3_url = "https:{}".format(
                     s['mediaResource']['dflt']['audioURL']
                 )
-                # apri mp3 object e download binary content sul file
+                # apri l'oggetto mp3 e download il binary content sul file
                 doc = requests.get(url=mp3_url)
+                doc.raise_for_status()
                 file_download = file if counter == 0 else "{0}({1}).mp3".format(
                     file.rsplit(".", 1)[0],
                     counter
