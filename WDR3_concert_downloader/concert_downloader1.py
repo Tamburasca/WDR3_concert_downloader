413ce3350aaa8b2bc00263058db42333e6e9de7f
Tamburasca <rtimmermann@astro.uni-bonn.de>
2024-08-17 22:38:44 +0200

    version 2.0.0 Seek regEx pattern

---
+++ WDR3_concert_downloader/concert_downloader1.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from pydantic import BaseModel, HttpUrl
+
+
+class TestURL(BaseModel):
+    url: HttpUrl
+
+
+def wdr3_scraper(
+        url: str,
+        file: str
+) -> int:
+    pattern = re.compile(r'"audioURL" : "(.*)"')
+    counter = 0
+    try:
+        if not file.endswith(".mp3"):
+            raise NameError
+        # verificare e tentare d'aprire url iniziale
+        response = requests.get(url=TestURL(url=url).url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, "html.parser")
+        for value in soup.find_all('script'):
+            mp3_url = re.findall(pattern, value.text)
+            if mp3_url:
+                sneak_mp3 = "https:{}".format(mp3_url[0])
+                # apri l'oggetto mp3 e download il binary content sul file
+                doc = requests.get(url=sneak_mp3)
+                doc.raise_for_status()
+                file_download = file if counter == 0 else "{0}({1}).mp3".format(
+                    file.rsplit(".", 1)[0],
+                    counter
+                )
+                with open(file_download, 'wb') as f:
+                    f.write(doc.content)
+                print("{1} downloaded to {0} successfully".format(
+                    file_download,
+                    sneak_mp3)
+                )
+                counter += 1
+        return 0
+    except NameError:
+        print("Error: filename '{}' is incorrect.".format(file))
+    except Exception as e:
+        print("Occurred error: {}".format(str(e)))  # Bih, chi camurrìa!
+    return 1
