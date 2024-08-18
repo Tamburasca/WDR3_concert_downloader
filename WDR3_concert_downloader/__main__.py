4864af4d902a9a81c9833cf50fc4a6a3614763c6
Tamburasca <rtimmermann@astro.uni-bonn.de>
2024-08-18 17:26:08 +0200

    version 2.0.0 Seek regEx pattern

--- WDR3_concert_downloader/__main__.py
+++ WDR3_concert_downloader/__main__.py
@@ -32,7 +32,7 @@ __author__ = "Dr. Ralf Antonius Timmermann"
 __copyright__ = ("Copyright (c) 2024, Dr. Ralf Antonius Timmermann "
                  "All rights reserved.")
 __credits__ = []
-__license__ = "BSD-3-Clause"
+__license__ = "BSD 3-Clause"
 __version__ = "2.0.0"
 __maintainer__ = "Dr. Ralf Antonius Timmermann"
 __email__ = "rtimmermann@astro.uni-bonn.de"
