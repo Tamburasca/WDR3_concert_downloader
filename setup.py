62e0117e690f93224094f619166625e87008a362
Tamburasca <rtimmermann@astro.uni-bonn.de>
2024-08-18 17:26:29 +0200

    version 2.0.0 Seek regEx pattern

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as f:
+    dependencies = [line.rstrip() for line in f]
+
+setuptools.setup(
+    name="WDR3 Concert Downloader",
+    version="2.0.0",
+    author="Dr. Ralf Antonius Timmermann",
+    author_email="rtimmermann@gmx.de",
+    description="Download appropriate mp3 to file, where's no download button",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tamburasca/WDR3_concert_downloader",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD 3-Clause",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='=3.10',
+    install_requires=dependencies,
+    license='BSD 3-Clause',
+)
